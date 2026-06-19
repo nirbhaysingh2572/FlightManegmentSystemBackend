2bce72e44fa60c649bedfe484cbb2f71739f0a0f
nirbhaysingh2572 <chhivalaha499@gmail.com>
2026-06-19 16:56:05 +0530

    added BookingService added subscribe message function in ampq

--- Bookingservice/src/utils/ampq.js
+++ Bookingservice/src/utils/ampq.js
@@ -25,25 +25,44 @@ function getChannel(){
 }
 
 
-async function publishMessage(channel, exchange, key, msg){
+async function publishMessage(channel, exchange, Binding_key, msg, Exchange_type = 'direct'){
     try{
-        await channel.assertExchange(exchange,'direct',{
+        await channel.assertExchange(exchange, Exchange_type,{
             durable:false
         });
-        await channel.publish(exchange, key, Buffer.from(msg) );
+        await channel.publish(exchange, Binding_key, Buffer.from(msg) );
     }
     catch(error){
         console.log("some error in publising the message :", error);
     }
 }
 
+async function subscribeMessage(service, channel, exchange, Binding_key, queue='', Exchange_type = 'direct'){
+    try{
+        await channel.assertExchange(exchange, Exchange_type,{
+            durable:false
+        });
+        const q = await channel.assertQueue(queue);
+        await channel.bindQueue(q.queue,exchange,Binding_key);
+
+        // now consume the message
+        await channel.consume(q.queue, async(msg)=>{
+            await service(msg);
+        },{
+            noAck:true
+        });
+    }
+    catch(error){
+        console.log("some error in subscribing message :", error);
+    }
+}
 
 
 module.exports = {
     connnectRabbitMQ,
     getChannel,
     publishMessage,
-
+    subscribeMessage,
 }
 
 
