b8b52d03020ab4ca44dd1aab47bf9a1d18df9b6c
nirbhaysingh2572 <chhivalaha499@gmail.com>
2026-06-22 15:30:35 +0530

    added ApiGatteway added middelware for different athentication

---
+++ ApiGatway/src/middelwares/auth-middelware.js
@@ -0,0 +1,161 @@
+const { StatusCodes } = require('http-status-codes');
+const axios = require('axios');
+
+const { ValidationError,ServiceError } = require('../utils/error/index');
+const { AUTH_SERVICE_PATH } = require('../config/server-config');
+
+
+async function authenticateUser(token){
+    try{
+        //authenticate user
+        const AUTHENTICATION_URL = `${AUTH_SERVICE_PATH}/api/v1/user/isAuthenticated`;
+
+        const result = await axios.get(AUTHENTICATION_URL,{
+                            headers:{
+                                'x-access-token': token
+                            }
+                        });
+                        
+        return result.data.data.userId;
+    }
+    catch(error){
+        if(error.name == "AxiosError" && error.response){
+            throw(
+                new ValidationError({
+                    message: error.response.data.message,
+                    explanation: error.response.data.error,
+                    statusCode: error.response.status
+                })
+            );
+        }
+
+        console.log("Authentication service is down !")
+        throw(
+            new ServiceError()
+        );
+    }
+}
+
+async function checkAdminRole(userId){
+     try{
+        //authenticate user
+        const IS_ADMIN_URL = `${AUTH_SERVICE_PATH}/api/v1/user/isAdmin/${userId}`;
+
+        const result = await axios.get(IS_ADMIN_URL);
+        
+        return result.data.data;
+    }
+    catch(error){
+        
+        if(error.name == "AxiosError" && error.response){
+            throw(
+                new ValidationError({
+                    message: error.response.data.message,
+                    explanation: error.response.data.error,
+                    statusCode: error.response.status
+                })
+            );
+        }
+
+        console.log("Authentication service is down !")
+        throw(
+            new ServiceError()
+        );
+    }
+}
+
+const isAuthenticated = async (req,res, next)=>{
+    try{
+        const token = req.headers && req.headers['x-access-token'];
+        if(!token){
+            throw(
+                new ValidationError({
+                    message:"Invalid Token !",
+                    explanation: "This servise require authentication and you have entered invalid token !"
+                })
+            );
+        }
+
+        await authenticateUser(token);
+        next(); 
+    }
+    catch(error){
+        return res.status(error.statusCode).json({
+            data: {}, 
+            sucess: false,
+            message:error.message,
+            error: error.explanation
+        });
+    }
+}
+
+const isAdmin = async (req,res,next)=>{
+    try{
+        const token = req.headers && req.headers['x-access-token'];
+        if(!token){
+            throw(
+                new ValidationError({
+                    message:"Invalid Token !",
+                    explanation: "This servise require authentication and you have entered invalid token !"
+                })
+            );
+        }
+
+        const userId =  await authenticateUser(token);
+        await checkAdminRole(userId);
+
+        next();
+    }
+    catch(error){
+        return res.status(error.statusCode).json({
+            data: {}, 
+            sucess: false,
+            message:error.message,
+            error: error.explanation
+        });
+    }
+}
+
+const checkUserId = async (req, res, next) => {
+    try{
+        const token = req.headers && req.headers['x-access-token'];
+        
+        if(!token){
+            throw(
+                new ValidationError({
+                    message:"Invalid Token !",
+                    explanation: "This servise require authentication and you have entered invalid token !"
+                })
+            );
+        }
+
+        const userId =  await authenticateUser(token);
+        const id = req.params.id;
+        if(id != userId ){
+            throw(
+                new ValidationError({
+                    message:"Anauthrized !",
+                    explanation: "You are not atherized for this action !"
+                })
+            );
+        }
+
+        next();
+    }
+    catch(error){
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            data: {}, 
+            sucess: false,
+            message:"Not Autherized!",
+            error: "user is not autherized for this requset!"
+        });
+    }
+}
+
+
+module.exports = {
+    isAuthenticated,
+    isAdmin,
+    checkUserId,
+
+}
